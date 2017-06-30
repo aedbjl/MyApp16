fabcca79d82db9db53bbf576dafeb49a6656f5a3
iii-user <iii-user@iii-userde-macbook-4.local>
2017-06-30 16:32:00 +0800

    k

--- MyApp16/ViewController.swift
+++ MyApp16/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let fmgr = FileManager.default
+    let home = NSHomeDirectory()
+    var appdata:String?
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urls = fmgr.urls(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask)
+        for i in 0..<urls.count{
+            print(urls[i].absoluteString)
+        }
+        
+        let home = NSHomeDirectory()
+        print (home)
+        
+//        let docuPath = home + "/Documents"
+//        let newdir = docuPath + "/dir1"
+//        let copydir = docuPath + "/dir2"
+        appdata = home + "/Documents/mydata"
+        if !fmgr.fileExists(atPath: appdata!){
+            do {
+                try fmgr.createDirectory(atPath: appdata!, withIntermediateDirectories: true, attributes: nil)
+                  print("create Dir")
+
+            }catch{
+                
+            }
+        }else {
+            print("dir ok")
+        }
+        
+        
+//        do {
+//           try fmgr.createDirectory(atPath: newdir, withIntermediateDirectories: true, attributes: nil)
+            
+////            try fmgr.copyItem(atPath: newdir, toPath: copydir)
+//            try fmgr.removeItem(atPath: copydir)
+//            print("ok")
+//            
+        
+//        }catch{
+//            print(error)
+//        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
